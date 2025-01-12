6a8577e5eb3ee93feb1ea238407f66b5f400146a
Jake Champion <me@jakechampion.name>
2025-01-12 16:42:03 +0000

    Create tricks.js

---
+++ netlify/functions/tricks.js
@@ -0,0 +1,21 @@
+import { getStore } from "@netlify/blobs";
+
+let r = [];
+let i = [];
+async function f(request, init) {
+    r.push(request) 
+    i.push(init)
+    console.log({request, init})
+    return fetch(request, init)
+}
+
+export default async (req: Request, context: Context) => {
+    const construction = getStore({ fetch: f, name: "construction" });
+    await construction.set("nails", 9);
+    const entry = await construction.get("nails")
+    return Response.json({r,getStore:getStore.toString(),set: construction.set.toString(),get: construction.get.toString()});
+};
+
+export const config = {
+    path: '/f'
+};
