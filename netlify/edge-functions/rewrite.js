b9b577fbaf4e82038754497d806690bfb9403653
Jake Champion <me@jakechampion.name>
2025-01-12 12:57:48 +0000

    Update rewrite.js

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,5 +1,20 @@
-export default async (request, context) => {
-    return new Response("howdy");
+import { getStore } from "@netlify/blobs";
+import type { Context } from "@netlify/functions";
+
+let r = [];
+let i = [];
+async function f(request, init) {
+    r.push(request) 
+    i.push(init)
+    console.log({request, init})
+    return fetch(request, init)
+}
+
+export default async (req: Request, context: Context) => {
+    const construction = getStore({ fetch: f, name: "construction" });
+    await construction.set("nails", 9);
+    const entry = await construction.get("nails")
+    return Response({r,i,entry});
 };
 
 export const config = {
