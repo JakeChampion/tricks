44fea68d5cabd00a918fa9f79090ce6c0b01824c
Jake Champion <me@jakechampion.name>
2025-01-12 13:20:36 +0000

    Update rewrite.ts

--- netlify/edge-functions/rewrite.ts
+++ netlify/edge-functions/rewrite.ts
@@ -14,7 +14,7 @@ export default async (req: Request, context: Context) => {
     const construction = getStore({ fetch: f, name: "construction" });
     await construction.set("nails", 9);
     const entry = await construction.get("nails")
-    return Response.json({r,getStore,set: construction.set,get: construction.get});
+    return Response.json({r,getStore,set: construction.set.toString(),get: construction.get.toString()});
 };
 
 export const config = {
