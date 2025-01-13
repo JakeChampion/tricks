0afbc544523f448a6ec67ae93a353335de4ad9b4
jake champion <jake.champion@netlify.com>
2025-01-13 16:43:30 +0000

    wow

--- netlify/functions/tricks.js
+++ netlify/functions/tricks.js
@@ -1,7 +1,15 @@
 import { getStore } from "@netlify/blobs";
 
+async function f(request, init) {
+    console.log({request, init})
+    return fetch(request, init)
+}
+
 export default async (req, context) => {
-    const construction = getStore("construction");
+    const construction = getStore({
+        fetch: f, 
+        name: "construction"
+    });
     await construction.set("nails", 9);
     const entry = await construction.get("nails")
     return new Response(entry);
